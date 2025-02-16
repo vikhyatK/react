62ec26a6ee2c39b87011466d5280ecff794da1c6
Vikhyat <vikhyat.kaushik@gmail.com>
2025-02-16 13:55:25 +0530

    React first project

---
+++ first-react/src/index.jsx
@@ -0,0 +1,17 @@
+function Index() { 
+    return (
+        <>
+            <img src="/src/assets/react.svg" width="40px" alt="React logo"></img>
+            <h1>Fun facts about React!</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walkie</li>
+                <li>Has well over 200k stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </>
+    )
+}
+
+export default Index
