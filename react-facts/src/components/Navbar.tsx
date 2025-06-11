3265ddaf70a9c51201559d74452e633f05455b1b
Vikhyat <vikhyat.kaushik@gmail.com>
2025-06-11 14:00:41 +0530

    Updated the Span tag content

--- react-facts/src/components/Navbar.tsx
+++ react-facts/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
             <header>
                 <nav>
                     <img src={logo} className='App-logo' alt='React Logo'></img>
-                    <span>ReactFacts</span>
+                    <span>ReactFact</span>
                 </nav>
             </header>
         </>
